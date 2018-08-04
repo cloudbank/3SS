22e1c067bf6222505123d86f46a418439851deb6
droid teahouse <sabine.g.vogel@gmail.com>
2018-08-04 14:50:25 -0700

    kotlin version
    hash is reverse of other due to endianness; can be used w direct buffer too

---
+++ kotlinDHash.kt
@@ -0,0 +1,41 @@
+  fun dhash(oldWidth: Int, oldHeight: Int, newWidth: Int, newHeight: Int, ib: IntArray): Long {
+
+            val newBitmapPixels = IntArray(newWidth * newHeight)
+            var x2: Int
+            var y2: Int
+            var index = 0
+            var hash: Long = 0
+
+            //buffer has been allocated for size already on java side
+            for (y in 0 until newHeight) {
+                for (x in 0 until newWidth) {
+                    x2 = x * oldWidth / newWidth
+                    if (x2 < 0)
+                        x2 = 0
+                    else if (x2 >= oldWidth)
+                        x2 = oldWidth - 1
+                    y2 = y * oldHeight / newHeight
+                    if (y2 < 0)
+                        y2 = 0
+                    else if (y2 >= oldHeight)
+                        y2 = oldHeight - 1
+                    newBitmapPixels[index] = ib[((y2 * oldWidth) + x2)]
+                    //same as : newBitmapPixels[(y * newWidth) + x] = previousData[(y2 * oldWidth) + x2];
+                    /////>>>>>> big endian
+                    if (index > 0) {
+                        if ((index) % newWidth != 0) {
+                            var pixel2 = newBitmapPixels[index]
+                            var pixel = newBitmapPixels[index - 1]
+                            pixel2 = (((pixel2 shr 24) and 0xff) * 0.299 + ((pixel2 shr 16) and 0xff) * 0.587 +
+                                    ((pixel2 shr 8) and 0xff) * 0.114).toInt()
+                            pixel = (((pixel shr 24) and 0xff) * 0.299 + ((pixel shr 16) and 0xff) * 0.587 +
+                                    ((pixel shr 8) and 0xff) * 0.114).toInt()
+                            hash = hash or (((pixel) < (pixel2)).toLong())
+                            hash = hash shl 1
+                        }
+                    }
+                    index++
+                }
+            }
+            return hash
+        }
