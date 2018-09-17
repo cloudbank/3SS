51a3a99d05e5e6488ccca15306844dd85cef288d
droid teahouse <sabine.g.vogel@gmail.com>
2018-09-16 17:00:40 -0700

    refactored but still slower than native

--- kotlinDHash.kt
+++ kotlinDHash.kt
@@ -1,9 +1,13 @@
-  fun dhash(oldWidth: Int, oldHeight: Int, newWidth: Int, newHeight: Int, ib: IntArray): Long {
 
-            val newBitmapPixels = IntArray(newWidth * newHeight)
+
+        fun dhash(oldWidth: Int, oldHeight: Int, newWidth: Int, newHeight: Int, ib: IntArray): Long {
+
+            //val newBitmapPixels = IntArray(newWidth * newHeight)
             var x2: Int
             var y2: Int
+            val end = (newWidth * newHeight) - 1;
             var index = 0
+            var firstpixel = 0
             var hash: Long = 0
 
             //buffer has been allocated for size already on java side
@@ -19,23 +23,32 @@
                         y2 = 0
                     else if (y2 >= oldHeight)
                         y2 = oldHeight - 1
-                    newBitmapPixels[index] = ib[((y2 * oldWidth) + x2)]
+                    //newBitmapPixels[index] = ib[((y2 * oldWidth) + x2)]
                     //same as : newBitmapPixels[(y * newWidth) + x] = previousData[(y2 * oldWidth) + x2];
                     /////>>>>>> big endian
-                    if (index > 0) {
-                        if ((index) % newWidth != 0) {
-                            var pixel2 = newBitmapPixels[index]
-                            var pixel = newBitmapPixels[index - 1]
-                            pixel2 = (((pixel2 shr 24) and 0xff) * 0.299 + ((pixel2 shr 16) and 0xff) * 0.587 +
-                                    ((pixel2 shr 8) and 0xff) * 0.114).toInt()
-                            pixel = (((pixel shr 24) and 0xff) * 0.299 + ((pixel shr 16) and 0xff) * 0.587 +
-                                    ((pixel shr 8) and 0xff) * 0.114).toInt()
-                            hash = hash or (((pixel) < (pixel2)).toLong())
-                            hash = hash shl 1
-                        }
+                    if (index % newWidth == 0) {
+                        firstpixel = ib[((y2 * oldWidth) + x2)];
+                    } else if ((index) % newWidth != 0) {
+                        var pixel2 = ib[y2 * oldWidth + x2]
+                        var pixel = firstpixel
+                        firstpixel = pixel2
+                        pixel2 = (((pixel2 shr 24) and 0xff) * 0.299 + ((pixel2 shr 16) and 0xff) * 0.587 +
+                                ((pixel2 shr 8) and 0xff) * 0.114).toInt()
+                        pixel = (((pixel shr 24) and 0xff) * 0.299 + ((pixel shr 16) and 0xff) * 0.587 +
+                                ((pixel shr 8) and 0xff) * 0.114).toInt()
+
+                        hash = (hash or ((pixel < pixel2).toLong()))
+                        if (index < end) {
+                            hash = (hash shl 1)
+                    }
                     }
                     index++
                 }
+
             }
+
             return hash
         }
+
+        inline fun Boolean.toLong() = if (this) 1L else 0L
+
